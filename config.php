98932bb6ae769f451784f85fc41b91aef95b65f4
Shivdhwaj Pandey <shivdhwaj@gmail.com>
2015-01-04 19:50:52 +0530

    Adding config file for Database configuration

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+	//Database configuration file
+	$servername = "localhost"; //Server name for your Database
+	$username = "username"; //Username for the Databse access
+	$password = "password"; //Password for the Databse access
+	$dbname = "todoApp"; //Database name for the appication
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+?>
