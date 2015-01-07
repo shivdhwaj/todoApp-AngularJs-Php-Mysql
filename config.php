b37179e833b06a6d6ae92526b7cfcbf59160b8fc
Shivdhwaj Pandey <shivdhwaj@gmail.com>
2015-01-08 00:33:41 +0530

    Separated included files page

--- config.php
+++ config.php
@@ -3,12 +3,13 @@
 	$servername = "localhost"; //Server name for your Database
 	$username = "username"; //Username for the Databse access
 	$password = "password"; //Password for the Databse access
-	$dbname = "todoApp"; //Database name for the appication
+	$dbname = "databasename"; //Database name for the appication
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
-	} 
+	}
+	include_once("includedFiles.php");	
 ?>
---
+++ includedFiles.php
@@ -0,0 +1,10 @@
+
+<!----Include JS files Here ------->
+<script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+<script type="text/javascript" src="js/angular.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+
+<!------Include CSS files Here ------>
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="css/bootstrap-theme.min.css" type="text/css">
