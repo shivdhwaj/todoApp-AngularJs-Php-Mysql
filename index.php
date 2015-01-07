b5c9f44a06fa641df0940cc0d96c27765392ca0e
Shivdhwaj Pandey <shivdhwaj@gmail.com>
2015-01-08 00:36:57 +0530

    Main page update View level

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<?php
-	echo "Hello to the first page for the todoApp";
-?>
+<?php include_once("config.php");?>
+<html>
+	<head>
+		<title>
+			todoApp-AngularJs-Php-Mysql
+		</title>
+	</head>
+	<body>
+		<!-------Header Navigation Bar Starts Here------------------>
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+			<div class="navbar-header">
+			  <a class="navbar-brand" href="#">
+				<img alt="Brand" src="images/todoAppIcon.png" width=150px height=50px style="margin-top:-10px;">
+			  </a>
+			</div>
+		  </div>
+		</nav>
+		<!-------Header Navigation Bar Ends Here------------------>
+		<div class="clearfix"></div>
+		<!-------Container for App Starts Here------------------>
+		<div class="container">
+	  		<div class="jumbotron">
+				<h1>todoApp Application</h1>      
+				<p>AngularJs,Php and Mysql</p>      			
+		  	</div>		  	
+	  	</div>
+		<!-------Container for App Ends Here------------------>
+	</body>
+</html>
