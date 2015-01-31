ac1956600428b4cac124fc691d99968b052c4f3c
shivdhwaj pandey <shivdhwaj@gmail.com>
2015-02-01 01:18:21 +0530

    Included the Angualr code file

--- includedFiles.php
+++ includedFiles.php
@@ -1,11 +1,11 @@
 
-<!----Include JS files Here ------->
+<!--Include JS files Here -->
 <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
 <script type="text/javascript" src="js/angular.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/todoCode.js"></script>
 
 
-<!------Include CSS files Here ------>
+<!--Include CSS files Here -->
 <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
 <link rel="stylesheet" href="css/bootstrap-theme.min.css" type="text/css">
